f8665727a50c9e4dc6a7e6e2d9e23f4d0731a28e
nhphong0104 <nhphong0104@gmail.com>
2021-08-11 19:36:13 +0700

    Update index.blade.php

--- platform/themes/icatgroup/views/index.blade.php
+++ platform/themes/icatgroup/views/index.blade.php
@@ -238,8 +238,7 @@
                 <div class="row">
                     <div class="col-md-10 col-md-offset-1">
                         <h2 class="text-uppercase line-bottom-double-line-centered mt-0">Dịch vụ <span class="text-theme-colored2">tài chính</span></h2>
-                        <p>Tín hiệu được các chuyên gia nhiều năm kinh nghiệm giao dịch ngoại
-                            hối và chứng khoán cung cấp hàng ngày với độ chính xác lên đến ≥ 70%. </p>
+                        <p>Chúng tôi đang cung cấp Dịch vụ cho các Nhà giao dịch trong hơn 3 năm nay và đã giúp hàng nghìn Khách hàng hài lòng cho đến nay. Mục tiêu của chúng tôi là cung cấp cho bạn dịch vụ chuyên nghiệp chất lượng và với tất cả kiến thức, nỗ lực và kinh nghiệm của chúng tôi hướng dẫn bạn trở thành một Nhà giao dịch thành công. </p>
                     </div>
                 </div>
             </div>
@@ -250,8 +249,8 @@
                             <a href="#" class="icon icon-gray icon-bordered bg-hover-theme-colored icon-circled icon-xl">
                                 <i class="fas fa-chart-line text-theme-colored font-48"></i>
                             </a>
-                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Gói tín hiệu</a></h4>
-                            <p class="">Tín hiệu được các chuyên gia nhiều năm kinh nghiệm giao dịch ngoại hối và chứng khoán cung cấp hàng ngày với độ chính xác lên đến ≥ 70%.</p>
+                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Tư vấn</a></h4>
+                            <p class="">Chúng tôi không chỉ cung cấp các giải pháp giúp bạn có được “kiến thức để đầu tư thông mình tiến tới tự do tài chính”, mà chúng tôi còn đồng hành cùng bạn thực hiện nó một cách tốt nhất, tiết kiệm chi phí và thời gian</p>
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -259,8 +258,8 @@
                             <a href="#" class="icon icon-gray icon-bordered bg-hover-theme-colored2 icon-circled icon-xl">
                                 <i class="fa fa-line-chart text-theme-colored2 font-48"></i>
                             </a>
-                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Zoom thực chiến</a></h4>
-                            <p class=""> Các buổi Live - Zoom trade live thực chiến thứ 4 – thứ 6 hàng tuần kiếm lợi nhuận trực tiếp do các chuyên gia IC&T GROUP tổ chức.</p>
+                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Signals</a></h4>
+                            <p class="">Với đội ngũ các nhà giao dịch nhiều kinh nghiệm và có hiệu suất giao dịch cao, Chúng tôi hiện đang cung cấp tín hiệu trên 29 cặp ngoại hối và Vàng với chi phí thấp và lợi nhuận cao trong ngày.</p>
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -268,8 +267,8 @@
                             <a href="#" class="icon icon-gray icon-bordered bg-hover-theme-colored icon-circled icon-xl">
                                 <i class="fa fa-pie-chart text-theme-colored font-48"></i>
                             </a>
-                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Hệ thống Copytrade</a></h4>
-                            <p class="">Hệ thống copytrade được vận hành bởi các chuyên gia nhiều năm giao dịch trên thị trường Tài chính luôn đảm bảo an toàn cho tài khoản của Khách hàng</p>
+                            <h4 class="icon-box-title text-uppercase"><a class="" href="#">Funds</a></h4>
+                            <p class="">Chúng tôi biết rằng rất khó để có thể có lợi nhuận ở thị trường tài chính một cách nhanh chóng, nên chúng tôi quyết định tạo Giải pháp Tất cả trong một và giúp cho giao dịch của bạn trở nên thú vị, có lợi nhuận và dễ dàng hơn bao giờ hết.</p>
                         </div>
                     </div>
                 </div>
