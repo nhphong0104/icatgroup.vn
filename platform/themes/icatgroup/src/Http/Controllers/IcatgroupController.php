8ba8294af724af273794e2aa7d1277e46d41868a
nhphong0104 <nhphong0104@gmail.com>
2021-11-22 11:04:19 +0700

    up

--- platform/themes/icatgroup/src/Http/Controllers/IcatgroupController.php
+++ platform/themes/icatgroup/src/Http/Controllers/IcatgroupController.php
@@ -3,7 +3,9 @@
 namespace Theme\Icatgroup\Http\Controllers;
 
 use Botble\Base\Http\Responses\BaseHttpResponse;
+use Botble\SeoHelper\SeoOpenGraph;
 use Botble\Theme\Http\Controllers\PublicController;
+use SeoHelper;
 
 class IcatgroupController extends PublicController
 {
@@ -37,6 +39,20 @@ class IcatgroupController extends PublicController
 
     public function goMarkets(){
         //return \Theme::scope('rescuitment')->render();
+
+        SeoHelper::setTitle('GO Markets')
+            ->setDescription('GO Markets đối tác giao dịch quỹ hàng đầu tại Úc');
+
+        $meta = new SeoOpenGraph;
+        $meta->setImage('https://icatgroup.vn/storage/chelsea.png');
+        $meta->setDescription('GO Markets đối tác giao dịch quỹ hàng đầu tại Úc');
+        $meta->setUrl('https://icatgroup.vn/go-markets');
+        $meta->setTitle('GO Markets');
+        $meta->setType('article');
+
+        SeoHelper::setSeoOpenGraph($meta);
+
+
         return \Theme::layout('landing_go')->scope('landing.gomarkets')->render();
     }
 }
